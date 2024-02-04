2e3cf0038b90793699a6ab4a30b0be906c03e929
ethanjones-git <73539012+ethanjones-git@users.noreply.github.com>
2024-02-04 11:23:21 -0800

    Update api_polypinion.py

--- api_polypinion.py
+++ api_polypinion.py
@@ -2,73 +2,114 @@
 MAIN API
 '''
 import pandas as pd
+import json
+from intr_sql import top_10
+from datetime import datetime
 
-def articles_and_ranks(art_path,rank_path):
+def articles_and_ranks():
+    '''
+    This takes 2 long-format tables, and merges them.
+
+    This also finds two articles under the same rank. If there are two, it indents them in the json.
+
+    :param article database path:
+    :param rank database path:
+    :return python dictionary:
+    '''
     #load from paths
-    df_articles = pd.read_csv(art_path)
-    df_rank = pd.read_csv(rank_path)
+    #df_articles = pd.read_csv(art_path)
+    #df_rank = pd.read_csv(rank_path)
 
     # data operation
-    df = df_rank.merge(df_articles,how='left',on='key_id')
-    out= df.to_dict(orient='records')
-    json_out = {}
+    #df = df_rank.merge(df_articles,how='left',on='key_id')
 
+    df,lst_ = top_10()
+
+
+    out= df.to_dict(orient='records')
+
+    json_out = {}
     # If one Rank has multiple articles, make them to a list
     for row in out:
-        rank,article = row['RANK'],row['ARTICLE']
+        rank,article = row['main_rank'],row['article']
 
         # Add the value to the dictionary entry
         if rank not in json_out:
             json_out[rank] = []
-        json_out[rank].append(row['ARTICLE'])
+        json_out[rank].append(row['article'])
+
     tup_art_lst = list(json_out.items())
 
-    # Same with dates
     json_out = {}
-
+    # If one Rank has multiple articles, make them to a list
     for row in out:
-        rank,article = row['RANK'],row['DATE']
+        rank, article = row['main_rank'], row['gist1']
 
         # Add the value to the dictionary entry
         if rank not in json_out:
             json_out[rank] = []
-        json_out[rank].append(row['DATE'])
+        json_out[rank].append(row['gist1'])
+
+    tup_gist1_lst = list(json_out.items())
+
+    json_out = {}
+    # If one Rank has multiple articles, make them to a list
+    for row in out:
+        rank, article = row['main_rank'], row['gist1']
+
+        # Add the value to the dictionary entry
+        if rank not in json_out:
+            json_out[rank] = []
+        json_out[rank].append(row['gist2'])
+
+    tup_gist2_lst = list(json_out.items())
+
+    json_out = {}
+    # If one Rank has multiple articles, make them to a list
+    for row in out:
+        rank, article = row['main_rank'], row['gist1']
+
+        # Add the value to the dictionary entry
+        if rank not in json_out:
+            json_out[rank] = []
+        json_out[rank].append(row['gist3'])
+
+    tup_gist3_lst = list(json_out.items())
+
+    json_out = {}
+    # If one Rank has multiple articles, make them to a list
+    for row in out:
+        rank, article = row['main_rank'], row['date']
+
+        # Add the value to the dictionary entry
+        if rank not in json_out:
+            json_out[rank] = []
+        json_out[rank].append(row['date'].to_pydatetime().strftime('%Y-%m-%d %H:%M:%S'))
+
     tup_dte_lst = list(json_out.items())
 
 
-    # Create the out
-    '''
-    
-    df__=df.drop_duplicates(subset = 'RANK', keep='first')
-    out= df__.to_dict(orient='records')
-    new_list = []
+    json_data = {"articles":[]}
     for row in out:
-        art = [y for x,y in tup_art_lst if x==row['RANK']]
-        dte = [y for x, y in tup_dte_lst if x == row['RANK']]
+        art = [y for x, y in tup_art_lst if x == row['main_rank']]
+        gist1_ = [y for x, y in tup_gist1_lst if x == row['main_rank']]
+        gist2_ = [y for x, y in tup_gist2_lst if x == row['main_rank']]
+        gist3_ = [y for x, y in tup_gist3_lst if x == row['main_rank']]
+        dte = [y for x, y in tup_dte_lst if x == row['main_rank']]
         json_fnl = {
-            'RANK':row['RANK'],
-            'KEY' : row['key_id'],
-            'HEADLINE': row['TITLE'],
-            'GIST': row['GIST'],
-            'date': row['DATE'],
-            'article': art[0]
+            'rank': row['main_rank'],
+            'key_id': row['key_id'],
+            'title': row['headline'],
+            'gist1': gist1_[0],
+            'gist2': gist2_[0],
+            'gist3': gist3_[0],
+            'category': row['cat'],
+            'created_at': row['date'].to_pydatetime().strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_at': dte[0],
+            'body': art[0],
+            'img': row['img']
         }
-        new_list.append(json_fnl)
-    '''
-
-    json_data = {"data":[]}
-    for row in out:
-        art = [y for x, y in tup_art_lst if x == row['RANK']]
-        dte = [y for x, y in tup_dte_lst if x == row['RANK']]
-        json_fnl = {
-            'RANK': row['RANK'],
-            'KEY': row['key_id'],
-            'HEADLINE': row['TITLE'],
-            'GIST': row['GIST'],
-            'date': row['DATE'],
-            'article': art[0]
-        }
-        json_data["data"].append(json_fnl)
+        json_data["articles"].append(json_fnl)
 
     return json_data
 
